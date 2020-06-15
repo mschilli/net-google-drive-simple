a191f0162620f813a222854819e154270937897d
Nicolas R <cpan@atoomic.org>
2020-06-15 08:43:38 -0600

    Call init once on download
    Fixes #35

--- lib/Net/Google/Drive/Simple.pm
+++ lib/Net/Google/Drive/Simple.pm
@@ -554,6 +554,8 @@ sub download {
 ###########################################
     my ( $self, $url, $local_file ) = @_;
 
+    $self->init();
+
     if ( ref $url ) {
         $url = $url->downloadUrl();
     }
